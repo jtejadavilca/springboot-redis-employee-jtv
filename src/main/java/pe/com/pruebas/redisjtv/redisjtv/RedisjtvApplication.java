a415453c75ebf7716c3e6834c047c4cf9f70dd82
Jose Tejada <jtejadavilca@gmail.com>
2019-06-08 15:02:46 -0500

    proyecto springBoot con redis para rest de employee en JavaExpert-CJAVA, fuente inicial

---
+++ src/main/java/pe/com/pruebas/redisjtv/redisjtv/RedisjtvApplication.java
@@ -0,0 +1,13 @@
+package pe.com.pruebas.redisjtv.redisjtv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisjtvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisjtvApplication.class, args);
+	}
+
+}
